6a0c06d275659e09564e63a9b9c2787cbeb25d9c
kaiya gunsauls <kaiyagunsauls@gmail.com>
2021-10-21 21:26:49 -0700

    added npm start

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT ||3000;
+const PORT = process.env.PORT || 3000;
 const htmlRoutes = require("./routes/htmlRoutes");
 const donutRoutes = require("./routes/donutRoutes");
 const logReq = require("./middleware/logRequest")
